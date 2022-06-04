cb0df63e0afd78ae12d2e8845cb6cac1663359b1
fede <federicoscheytt@gmail.com>
2022-06-04 12:48:51 -0300

    Commit TP2

---
+++ TP2/TP2.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file TP2.cpp
+ * @author Federico Scheytt (federico.scheytt@gmail.com)
+ * @version 0.1
+ * @date 2022-06-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(){
+
+    cout << "Hello world!" << endl;
+
+    return 0;
+}
